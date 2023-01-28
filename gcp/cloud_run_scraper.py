8051e8778f02fbf0ff6fdc0f96d52ff364c7374c
Nikul Patel <nikulpatel3141@users.noreply.github.com>
2023-01-28 08:57:56 +0000

    add cloud run scraping script

---
+++ gcp/cloud_run_scraper.py
@@ -0,0 +1,106 @@
+"""Script to run as a `Google Cloud Run Job`. Expects all parameters to be passed as env vars
+since this is how Cloud Run Jobs accept parameters
+
+Scrape a subset of tickers in the passed file based on the CLOUD_RUN_TASK_INDEX env var.
+See: https://cloud.google.com/run/docs/quickstarts/jobs/build-create-python
+
+Usage:
+
+```bash
+NUM_TASKS=20
+
+gcloud beta run jobs create job-scraper \
+    --image gcr.io/PROJECT_ID/scraping-job \
+    --tasks ${NUM_TASKS} \
+    --set-env-vars TICKER_FILE='gs://path/to/ticker/file.txt' \
+    --max-retries 1 \
+    --region REGION
+```
+"""
+from distutils.log import info
+import os
+import logging
+from typing import Sequence
+
+import pandas as pd
+import numpy as np
+
+from etf_scraper.main import scrape_holdings
+
+# save parameters
+TICKER_FILE = os.getenv("TICKER_FILE")
+SAVE_DIR = os.getenv("SAVE_DIR")
+SAVE_FMT = os.getenv("SAVE_FMT", "parquet")
+
+# query parameters
+START_DATE = os.getenv("START_DATE", "")
+END_DATE = os.getenv("END_DATE", "")
+MONTH_ENDS = bool(os.getenv("MONTH_ENDS", False))  # True if set, otherwise False
+TRADING_DAYS = bool(os.getenv("TRADING_DAYS", False))
+OVERWRITE = bool(os.getenv("OVERWRITE", False))
+EXCHANGE = os.getenv("EXCHANGE", "NYSE")
+
+# task parameters
+NUM_THREADS = int(os.getenv("NUM_THREADS", 10))
+
+TASK_COUNT = int(os.getenv("CLOUD_RUN_TASK_COUNT", 1))
+TASK_INDEX = int(os.getenv("CLOUD_RUN_TASK_INDEX", -1))
+TASK_ATTEMPT = int(os.getenv("CLOUD_RUN_TASK_ATTEMPT"))
+
+logger = logging.getLogger(__name__)
+
+
+def get_ticker_block(
+    ticker_file: str, task_index: int, num_tasks: int
+) -> Sequence[str]:
+    """Read the ticker file, split into NUM_TASKS blocks and returns the
+    block at task_index. If None given then returns all tickers
+    """
+    tickers = pd.read_csv(ticker_file, header=None).to_numpy().reshape(-1)
+
+    if len(tickers) == 0:
+        logger.info(f"No tickers found at {ticker_file}, exiting")
+        return []
+
+    if task_index == -1:
+        logger.info(f"No CLOUD_RUN_TASK_INDEX env var set so using all tickers")
+        return tickers
+
+    block_size = np.ceil(len(tickers) / int(num_tasks))
+    return tickers[block_size * task_index : block_size * (task_index + 1)]
+
+
+def main():
+    logger.info(
+        f"Starting attempt {TASK_ATTEMPT} fors task {TASK_INDEX} out of {TASK_COUNT}"
+    )
+
+    if not TICKER_FILE:
+        raise RuntimeError(
+            f"No TICKER_FILE env var set, can't retrieve tickers to query"
+        )
+
+    tickers_to_query = get_ticker_block(TICKER_FILE, TASK_INDEX, TASK_COUNT)
+
+    if not len(tickers_to_query):
+        logger.info(f"No tickers to query")
+        return
+
+    out = scrape_holdings(
+        tickers_to_query,
+        START_DATE,
+        END_DATE,
+        MONTH_ENDS,
+        TRADING_DAYS,
+        OVERWRITE,
+        SAVE_DIR,
+        SAVE_FMT,
+        NUM_THREADS,
+        EXCHANGE,
+    )
+    num_scraped = [1 for k in out.values() if "error" not in k]
+    logger.info(f"Scraped {num_scraped} holdings")
+
+
+if __name__ == "__main__":
+    main()
