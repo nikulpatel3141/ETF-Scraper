c95916fc77786d6fa7a6c52898a318b9e447453b
Nikul Patel <nikulpatel3141@users.noreply.github.com>
2023-03-25 20:12:06 +0000

    switch to saving on GCS

--- gcp/run_all.sh
+++ gcp/run_all.sh
@@ -2,26 +2,8 @@
 
 cd "$(dirname "$0")"
 
-git config --global user.email "gcp@gcp.gcp"
-git config --global user.name "GCP"
+python3 cloud_run_scraper.py
 
-TMP_OUT_DIR='/tmp/etf_scraper_output'
-
-mkdir ${TMP_OUT_DIR}
-export SAVE_DIR=${TMP_OUT_DIR}/output.json
-
-# python3 cloud_run_scraper.py
-
-# python3 bigquery_update.py
+python3 bigquery_update.py
 
 python3 calculate_flows.py # outputs to SAVE_DIR
-
-python3 setup_gh.py
-
-cd ${TMP_OUT_DIR}
-
-git init
-git remote add origin ${GH_OUT_REPO_URL}
-git add .
-git commit -m "Cloud Run scheduled run $(date)"
-git push origin master
