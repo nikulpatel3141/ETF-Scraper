cbd0775c5a84f162285fc5af9c922578bca3c400
Nikul Patel <nikulpatel3141@users.noreply.github.com>
2023-03-25 19:19:33 +0000

    ssh config bugfix

--- gcp/setup_gh.py
+++ gcp/setup_gh.py
@@ -1,6 +1,7 @@
 """Copy deploy keys + config from GCS for pushing output to GH"""
 
 import os
+import logging
 from pathlib import Path
 
 from google.cloud import storage
@@ -8,11 +9,19 @@ from google.cloud import storage
 
 SSH_CRED_URI = os.getenv("SSH_CRED_URI")
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] [%(filename)16s:%(lineno)4d:%(levelname)8s] - %(message)s",
+)
+
+logger = logging.getLogger(__name__)
+
 
 def main():
-    ssh_config_dir = os.path.expanduser("~/.sssh")
+    ssh_config_dir = os.path.expanduser("~/.ssh")
     os.makedirs(ssh_config_dir, exist_ok=True)
 
+    logger.info(f"Attempting to save contents of {SSH_CRED_URI} to {ssh_config_dir}")
     _, bucket, *blob = Path(SSH_CRED_URI).parts
     blob = "/".join(blob)
 
@@ -21,7 +30,9 @@ def main():
 
     for f in bucket.list_blobs(prefix=blob):
         filename = Path(f.name).name
-        f.download_to_filename(Path(ssh_config_dir).joinpath(filename))
+        out_path = Path(ssh_config_dir).joinpath(filename)
+        f.download_to_filename(out_path)
+        logger.info(f"Saved {filename} to {str(out_path)}")
 
 
 if __name__ == "__main__":
